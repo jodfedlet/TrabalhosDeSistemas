5b77340b63f535a9696fe5b13a06db22987670a4
Jod Fedlet PIERRE <fedletpierre15@gmail.com>
2017-11-28 14:25:06 -0200

    Update triangulo.v

--- TrabalhosDeSistemas/triangulo.v
+++ TrabalhosDeSistemas/triangulo.v
@@ -37,117 +37,78 @@ module sing(
 
 endmodule
 
-module PointInTriangle(
-    input clk,
-    input [11:0] PTX,
-    input [11:0] PTY,
+module TesteTriangulo (
+    input [11:0] Ponto1X,
+    input [11:0] Ponto1Y,
 
-    output inTriangle
-);
-    reg [11:0] P1X;
-    reg [11:0] P1Y;
+    input [11:0] Ponto2X,
+    input [11:0] Ponto2Y,
 
-    reg [11:0] P2X;
-    reg [11:0] P2Y;
+    input [11:0] Ponto3X,
+    input [11:0] Ponto3Y,
 
-    reg [11:0] P3X;
-    reg [11:0] P3Y;
+    input [11:0] PontoTX,
+    input [11:0] PontoTY,
 
-    reg [2:0] count = 0;
+    output dentro
+    );
 
-    wire sin1;
-    wire sin2;
-    wire sin3;
+    wire sinal1;
+    wire sinal2;
+    wire sinal3;
 
-    sing S1(PTX, PTY, P1X, P1Y, P2X, P2Y, sin1);
-    sing S2(PTX, PTY, P2X, P2Y, P3X, P3Y, sin2);
-    sing S3(PTX, PTY, P3X, P3Y, P1X, P1Y, sin3);
+    assign dentro = (sinal1 == 1 && sinal2 == 1 && sinal3 == 1) ? 1:0;
 
-    assign inTriangle = (sin1 == sin2 && sin2 == sin3) ? 1 : 0;
-
-    always @ (clk) begin
-            if(count == 0) begin
-                P1X = -6;
-                P1Y = -1;
-
-                P2X =  3;
-                P2Y =  3;
-
-                P3X =  3;
-                P3Y = -3;
-
-                count <= count + 1;
-            end
-
-            if(count == 1) begin
-                P1X =  20;
-                P1Y = -2;
-
-                P2X =  -1;
-                P2Y =   8;
-
-                P3X =  20;
-                P3Y =  12;
-
-                count <= count + 1;
-            end
-
-            if(count == 2) begin
-                P1X =  -17;
-                P1Y =  -4;
-
-                P2X =     5;
-                P2Y =   15;
-
-                P3X =  -43;
-                P3Y =   20;
-
-                count <= 0;
-            end
-    end
+    sinal S1(Ponto1X, Ponto1Y, Ponto2X, Ponto2Y, PontoTX, PontoTY, sinal1);
+    sinal S2(Ponto2X, Ponto2Y, Ponto3X, Ponto3Y, PontoTX, PontoTY, sinal2);
+    sinal S3(Ponto3X, Ponto3Y, Ponto1X, Ponto1Y, PontoTX, PontoTY, sinal3);
 
 endmodule
 
-module test;
+module Teste;
+    reg [11:0] Ponto1X;
+    reg [11:0] Ponto1Y;
 
-    reg [11:0] PTX;
-    reg [11:0] PTY;
+    reg [11:0] Ponto2X;
+    reg [11:0] Ponto2Y;
 
-    reg clk;
+    reg [11:0] Ponto3X;
+    reg [11:0] Ponto3Y;
 
-    wire InTriangle;
+    reg [11:0] PontoTX;
+    reg [11:0] PontoTY;
 
-    PointInTriangle P(clk , PTX, PTY, InTriangle);
+    wire Dentro;
 
+    TesteTriangulo A(Ponto1X, Ponto1Y, Ponto2X, Ponto2Y, Ponto3X, Ponto3Y, PontoTX, PontoTY, Dentro);
 
-    initial begin
-        $dumpvars(0, P);
-        #1
-        clk <= 1;
-        PTX <= -3;
-        PTY <= 3;
-        #1
-        PTX <= -14;
-        PTY <=  12;
-        #10
+    initial
+        begin
+           $dumpvars(0,A);
+           #1
 
-        #1
-        clk <= 0;
-        PTX <= -3;
-        PTY <= 3;
-        #1
-        PTX <= -14;
-        PTY <=  12;
-        #10
+           Ponto1X <= 10;
+           Ponto1Y <= 10;
+           Ponto2X <= 30;
+           Ponto2Y <= 10;
+           Ponto3X <= 20;
+           Ponto3Y <= 30;
+           PontoTX <= 15;
+           PontoTY <= 15;
 
-        #1
-        clk <= 1;
-        PTX <= -3;
-        PTY <= 3;
-        #1
-        PTX <= -14;
-        PTY <=  12;
-        #10
-        $finish;
-    end
+           #1
+           PontoTX <= 15;
+           PontoTY <= 15;
+           #1
+           PontoTX <= 9;
+           PontoTY <= 15;
+           #1
+           PontoTX <= 10;
+           PontoTY <= 11;
+           #1
+           PontoTX <= 30;
+           PontoTY <= 11;
+           #40
+           $finish;
+        end
 endmodule
